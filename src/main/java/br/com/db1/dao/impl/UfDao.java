3d3f7a3ddce6e3b6ce13e4c7319fb947b198ab32
Fabio <Fabiovolponi@hotmail.com>
2018-09-05 16:56:15 -0300

    Ta dificultando mais ainda

--- src/main/java/br/com/db1/dao/impl/UfDao.java
+++ src/main/java/br/com/db1/dao/impl/UfDao.java
@@ -3,6 +3,7 @@ package br.com.db1.dao.impl;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.management.Query;
 import javax.persistence.EntityManager;
 
 import br.com.db1.dao.DAO;
@@ -22,18 +23,20 @@ public class UfDao implements DAO<Uf> {
 	}
 
 	public Uf findById(Integer id) {
-
-		return null;
-	}
+		Query query = manager.createQuery("from uf where id = :pId");
+		query.setParameter("pId",id);
+		return (Uf) query.getSingleResult();
+	}	
 
 	public List<Uf> findByName(String name) {
-
-		return null;
+		Query query = manager.createQuery("from uf where nome like = :pNome");
+		query.setParameter("pNome","%"+name+"%");
+		return (Uf) query.getResultList();
 	}
 
-	public boolean save(Uf t) {
-
-		return false;
+	public boolean save(Uf uf) {
+		manager.persist(uf);
+		return true;
 	}
 
 	public boolean delete(Integer id) {
